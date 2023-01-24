ec29702393a18850cbbb48bfc4fe8ca52990059c
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-01-24 11:43:45 -0300

    feat: saves user in memory

--- src/server.js
+++ src/server.js
@@ -1,13 +1,21 @@
 import http from "node:http";
 
+const users = [];
+
 const server = http.createServer((request, response) => {
   const { method, url } = request;
 
   if (method === "GET" && url === "/users")
-    return response.end("Listagem de usuários");
+    return response.end(JSON.stringify(users));
 
   if (method === "POST" && url === "/users")
-    return response.end("Criação de usuários");
+    users.push({
+      id: 1,
+      name: "John Doe",
+      email: "johndoe@example.com",
+    });
+
+  return response.end("Criação de usuários");
 
   return response.end("Hello world!");
 });
