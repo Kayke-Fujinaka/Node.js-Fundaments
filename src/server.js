b23cb8404456f3036133ba9e8e3d52d64b608962
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-01-26 12:37:43 -0300

    feat: request body in JSON

--- src/server.js
+++ src/server.js
@@ -1,9 +1,22 @@
+import { randomUUID } from "node:crypto";
 import http from "node:http";
 
 const users = [];
 
-const server = http.createServer((request, response) => {
+const server = http.createServer(async (request, response) => {
   const { method, url } = request;
+
+  const buffers = [];
+
+  for await (const chunk of request) {
+    buffers.push(chunk);
+  }
+
+  try {
+    request.body = JSON.parse(Buffer.concat(buffers).toString());
+  } catch {
+    request.body = null;
+  }
 
   if (method === "GET" && url === "/users")
     return response
@@ -11,10 +24,12 @@ const server = http.createServer((request, response) => {
       .end(JSON.stringify(users));
 
   if (method === "POST" && url === "/users") {
+    const { name, email } = request.body;
+
     users.push({
-      id: 1,
-      name: "John Doe",
-      email: "johndoe@example.com",
+      id: randomUUID(),
+      name,
+      email,
     });
 
     return response.writeHead(201).end();
