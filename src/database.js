33c2c5e2fe94cd50e0b9886513b461d353f999e7
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-01-27 11:22:26 -0300

    feat: persists database

--- src/database.js
+++ src/database.js
@@ -1,5 +1,23 @@
+import fs from "node:fs/promises";
+
+const databasePath = new URL("../db.json", import.meta.url);
+
 export class Database {
   #database = {};
+
+  constructor() {
+    fs.readFile(databasePath, "utf-8")
+      .then((data) => {
+        this.#database = JSON.parse(data);
+      })
+      .catch(() => {
+        this.#persist();
+      });
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database, null, 2));
+  }
 
   select(table) {
     const data = this.#database[table] ?? [];
@@ -8,10 +26,12 @@ export class Database {
 
   insert(table, data) {
     if (Array.isArray(this.#database[table])) {
-      return this.#database[table].push(data);
+      this.#database[table].push(data);
+    } else {
+      this.#database[table] = [data];
     }
 
-    this.#database[table] = [data];
+    this.#persist();
 
     return data;
   }
